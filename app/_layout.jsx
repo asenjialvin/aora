521712b3cbac8c47b3170bd95a7892464d2f9fb5
asenjialvin <asenjialvin@gmail.com>
2024-09-16 15:32:46 +0300

    Appwrite upload configuration

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,11 +2,7 @@ import { useEffect } from "react";
 import { useFonts } from "expo-font";
 import "react-native-url-polyfill/auto";
 import { SplashScreen, Stack } from "expo-router";
-
 import GlobalProvider from "../context/GlobalProvider";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -8,7 +8,7 @@ const CustomButton = ({
   isLoading,
 }) => {
   return (
-    <TouchableOpacity  /* Dynamic Template String */
+    <TouchableOpacity
       onPress={handlePress}
       activeOpacity={0.7}
       className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
